46d7f5d286db7353e9161c3e6db2bf9044a616ec
haben0 <80583582+haben0@users.noreply.github.com>
2022-02-11 19:44:25 +0000

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,32 @@
 import React from "react";
-
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Header = () => {
     return (
-  <header>
-   
-        <h1>Header</h1>
-
+  <header className="header-area header-sticky">
+       <Container>
+            <Row>    
+                <Col>
+                    <nav className="main-nav">
+                        <a href="index.html" className="logo">
+                            Habesha City
+                        </a>
+                        <ul className="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="events.html">Events</a></li>
+                            <li class="scroll-to-section"><a href="#about">About us</a></li>                                                                        
+                            <li class="scroll-to-section"><a href="#contact">Contact Us</a></li>                           
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                    </nav>        
+                </Col>             
+            </Row>
+        </Container>
   </header>
-
     );
 }
 
