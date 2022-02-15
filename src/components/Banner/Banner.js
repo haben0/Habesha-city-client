fbeca87eb53f077a4450f188914148213f406c94
haben0 <80583582+haben0@users.noreply.github.com>
2022-02-15 21:08:08 +0000

    bug fix

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { Container, Row, Col } from 'react-bootstrap';
 import vid from '../../assets/videos/habesha_sefer1.mp4';
 
 const Banner = () => {
@@ -20,7 +18,7 @@ const Banner = () => {
                             <h2>Welcome to Habesha City</h2>
                             <p>We are a Habesha community at the heart of Manchester exicited to present you with a number of event to put on your calendar. We aim to build a city we would love to live in. So what are you waiting for, join us and be part of something special.</p>
                         <div className="main-button-red">
-                        <div className="scroll-to-section"><a href="#events">View Upcoming Events</a></div>
+                        <div className="scroll-to-section"><a href="events.html">View Upcoming Events</a></div>
                         </div>
                     </div>
                 </Col>
